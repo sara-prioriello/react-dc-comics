120a38bef7ab651d1165637a5fb465f157254a97
sara-prioriello <saraprioriello6@gmail.com>
2026-04-18 11:51:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Header from './components/AppHeader'
+import Main from './components/AppMain'
+import Footer from './components/AppFooter'
+
+export default function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+
